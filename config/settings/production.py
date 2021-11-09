55e43a4bb161cbc49cbda444b737074888143a46
Lincoln <me@lincolnh0.com>
2021-11-09 23:03:32 +0000

    Add s3 storage solution

--- config/settings/production.py
+++ config/settings/production.py
@@ -56,7 +56,19 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+LINODE_BUCKET = env("LINODE_BUCKET")
+LINODE_BUCKET_REGION = env("LINODE_BUCKET_REGION")
+LINODE_BUCKET_ACCESS_KEY = env("LINODE_BUCKET_ACCESS_KEY")
+LINODE_BUCKET_SECRET_KEY = env("LINODE_BUCKET_SECRET_KEY")
+
+AWS_S3_ENDPOINT_URL = f"https://{LINODE_BUCKET_REGION}.linodeobjects.com"
+AWS_ACCESS_KEY_ID = LINODE_BUCKET_ACCESS_KEY
+AWS_SECRET_ACCESS_KEY = LINODE_BUCKET_SECRET_KEY
+AWS_S3_REGION_NAME = LINODE_BUCKET_REGION
+AWS_S3_USE_SSL = True
+AWS_STORAGE_BUCKET_NAME = LINODE_BUCKET
 # MEDIA
 # ------------------------------------------------------------------------------
 

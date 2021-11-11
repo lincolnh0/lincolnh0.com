a90675588d863c70f607ac413d73a38f5e48be1b
Lincoln <me@lincolnh0.com>
2021-11-11 00:08:37 +0000

    Added Browse by year functionality on blog

--- lincolnh0_com/blog/views.py
+++ lincolnh0_com/blog/views.py
@@ -22,15 +22,23 @@ class BlogListView(ListView):
     paginate_by = 12
 
     def get_queryset(self):
-        projects = (
-            Post.objects.filter(is_published=True).order_by("created").reverse()
-        )
+        projects = Post.objects.filter(is_published=True)
+        year = self.request.GET.get("year")
+        if year:
+            projects = projects.filter(created__year=year)
+        projects = projects.order_by("created").reverse()
+
         return projects
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Blog posts"
-        context["tag_line"] = "Intermittent and unsolicited updates on anything and everything."
+        year = self.request.GET.get("year")
+        if year:
+            context["title"] += " in " + str(year)
+        context["tag_line"] = "Intermittent updates on anything and everything."
+
+        context["years"] = Post.objects.dates("created", "year", order="DESC")
         return context
 
 

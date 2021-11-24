d7f3f59dd3d3da9a1e96936d78870112dc9edaf5
Lincoln <me@lincolnh0.com>
2021-11-24 10:54:06 +0000

    Add blog form tests

--- lincolnh0_com/blog/tests/base.py
+++ lincolnh0_com/blog/tests/base.py
@@ -1,16 +1,19 @@
 from django.test import TestCase
+from lincolnh0_com.users.models import User
 from ..models import Post
 
 
 class PostTestCase(TestCase):
     def setUp(self):
         Post.objects.create(
-            title="My test blog",
-            body="<p>Test content</p>",
-            is_published=False
+            title="My test blog", body="<p>Test content</p>", is_published=False
         )
         Post.objects.create(
             title="My published test blog",
             body="<p>Test content</p>",
-            is_published=True
+            is_published=True,
         )
+
+        self.user = User.objects.create(username="testuser")
+        self.user.set_password("12345")
+        self.user.save()
---
+++ lincolnh0_com/blog/tests/test_forms.py
@@ -0,0 +1,74 @@
+from django.test import Client
+from django.urls import resolve, reverse
+
+from .base import PostTestCase
+from ..models import Post
+
+
+class PostFormTestCase(PostTestCase):
+    def test_posts_form_response(self):
+        """Posts form urls redirect when user is not logged in."""
+        test_post = Post.objects.get(title="My test blog")
+        client = Client()
+        update_response = client.get(
+            reverse("blog:update", kwargs={"pk": test_post.id})
+        )
+        self.assertRedirects(
+            update_response,
+            reverse("account_login")
+            + "?next="
+            + reverse("blog:update", kwargs={"pk": test_post.id}),
+        )
+
+        create_response = client.get(reverse("blog:create"))
+        self.assertRedirects(
+            create_response,
+            reverse("account_login") + "?next=" + reverse("blog:create"),
+        )
+
+    def test_create_form_submit(self):
+        """Post create form submits normally."""
+        client = Client()
+        client.force_login(self.user, backend=None)
+        url = reverse("blog:create")
+        form_response = client.post(
+            path=url,
+            data={
+                "title": "new project",
+                "body": "<p>Body Text</p>",
+                "is_published": True,
+            },
+        )
+        self.assertRedirects(
+            form_response,
+            expected_url=reverse(
+                "blog:detail",
+                kwargs={
+                    "slug": "new-project",
+                },
+            ),
+        )
+
+    def test_update_form_submit(self):
+        """Post update form submits normally."""
+        test_post = Post.objects.get(title="My test blog")
+        client = Client()
+        client.login(username="testuser", password="12345")
+        url = reverse("blog:update", kwargs={"pk": test_post.id})
+        form_response = client.post(
+            path=url,
+            data={
+                "title": test_post.title,
+                "body": test_post.body,
+                "is_published": test_post.is_published,
+            },
+        )
+        self.assertRedirects(
+            form_response,
+            expected_url=reverse(
+                "blog:detail",
+                kwargs={
+                    "slug": test_post.slug,
+                },
+            ),
+        )

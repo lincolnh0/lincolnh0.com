37f5352c5b449bf2aaf886ad757edd90db3b8919
Lincoln <me@lincolnh0.com>
2022-05-13 23:14:08 +0100

    Update SECURE_HSTS_SECONDS

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,5 @@
 from .base import *  # noqa
-from .base import env
+from .base import INSTALLED_APPS, env
 
 # GENERAL
 # ------------------------------------------------------------------------------
@@ -42,7 +42,7 @@ CSRF_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # TODO: set this to 60 seconds first and then to 518400 once you prove the former works
-SECURE_HSTS_SECONDS = 60
+SECURE_HSTS_SECONDS = 518400
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
     "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True
